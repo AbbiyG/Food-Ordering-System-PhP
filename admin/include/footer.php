27f7db5c8765706ae794ab0a82c03708d592a7fa
Abbiy Gautam <139056983+AbbiyG@users.noreply.github.com>
2023-10-06 20:18:02 +0545

    Add files via upload

---
+++ admin/include/footer.php
@@ -0,0 +1,1 @@
+ <footer class="footer" style="text-align: center;">Foodies Center © 2023 - Online Food Ordering System Developer - <a href="https://www.facebook.com/abbiygautam">Abhishek Gautam</a> </footer>
